77cbce87e52ce6788a937209588c049eb72f9135
Sidolbert <cyril.ENRICI@cote-azur.cci.fr>
2021-05-20 14:11:46 +0200

    modification du commentaire d'entrée

--- src/algo7/Algo7.java
+++ src/algo7/Algo7.java
@@ -10,7 +10,7 @@ Si l'algo trouve le nombre en moins de 5 essais vous perdez sinon vous gagnez
  */
 public class Algo7 {
 	public static void main(String[] args) {
-		//début du programme
+		//fonction principale du programme
 		int min=0, max=100, answer;
 		System.out.println("Bienvenue ! Pensez à un nombre entre "+ min +" et " + max + ", je vais essayer de le deviner");
 		System.out.println("Répondez Higher/Lower/Victory");
